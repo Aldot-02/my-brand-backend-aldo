c4bedb2531eb3a76a4b87a7563c8fc77e94efccc
Aldot-02 <aldotwizerimana1@gmail.com>
2024-03-12 16:45:38 +0200

    maintaining the original functionality

--- Server/Routes/UserRoute.ts
+++ Server/Routes/UserRoute.ts
@@ -75,7 +75,7 @@ router.get('/profile', isAuthenticated, getProfile);
  *       '500':
  *         description: Internal server error
  */
-router.get('/', isAuthenticated, getAllUsers);
+router.get('/', isAuthenticated, isAdmin, getAllUsers);
 
 /**
  * @openapi
