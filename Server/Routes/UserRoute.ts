761ccb0814d720149d278b725d4f9b0507803443
Aldot-02 <aldotwizerimana1@gmail.com>
2024-03-13 14:54:02 +0200

    protecting user routes

--- Server/Routes/UserRoute.ts
+++ Server/Routes/UserRoute.ts
@@ -75,7 +75,7 @@ router.get('/profile', isAuthenticated, getProfile);
  *       '500':
  *         description: Internal server error
  */
-router.get('/', getAllUsers);
+router.get('/', isAuthenticated, isAdmin, getAllUsers);
 
 /**
  * @openapi
